789c18d9926e623815dae7cfb2911793b643251b
Ashley1207 <13133821409@163.com>
2020-04-15 23:50:37 +0800

    editAnddelete-interface

--- backend/src/main/java/com/xjtu/backend/controller/TaskController.java
+++ backend/src/main/java/com/xjtu/backend/controller/TaskController.java
@@ -13,9 +13,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.security.cert.PKIXRevocationChecker.Option;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +53,35 @@ public class TaskController {
         
         return ResponseEntity.of(taskService.findTask(id));
     }
+
+    @PostMapping(consumes = "application/json",produces = "application/json")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        //TODO: process POST request
+        taskService.saveTask(task);
+        URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/tasks/{id}")
+                      .buildAndExpand(task.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+    /**
+     * postmapping:倾向于添加信息
+     * putmapping:倾向于更新信息
+     */
+    @PutMapping(path="/{id}",consumes = "application/json",produces = "application/json")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        //TODO: process PUT request
+        Optional<Task> updateTask=taskService.updateTask(new Task(id,task.getContent()));
+        
+        return ResponseEntity.of(updateTask);
+    }
     
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity deleteTask(@PathVariable Long id){
+        Optional<Task> deleteTask=taskService.deleteTask(id);
+        if(deleteTask.isPresent()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 
 }
